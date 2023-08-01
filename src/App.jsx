84d0984218a5ad5dfee95f19770da2c5190774a5
EstebanMontalvo99 <estebanmontalvo99@hotmail.com>
2023-07-31 21:38:45 -0500

    Adding pagination

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Users from './components/Users';
@@ -8,23 +8,29 @@ import useFetch from "./hooks/useFetch";
 
 function App() {
 
+  const [pagNumb, setPagNumb] = useState(0);
   const { users, getAllUsers } = useFetch();
   useEffect(() => {
     getAllUsers();
 
   }, []);
-  console.log(users);
+  const paginationUsers = [];
+  const paginationLength = 6;
+  for (let i = 0; i < users?.length; i += paginationLength) {
+    const sixUsers = users.slice(i, i + paginationLength);
+    paginationUsers.push(sixUsers);
+  }
 
   return (
     <div className='main__container'>
       <Header />
       <Searcher />
       <section className='users__container'>
-        {users?.map(user => (
+        {paginationUsers[pagNumb]?.map(user => (
           <Users key={user.uid} user={user} />
         ))}
       </section>
-      <Footer />
+      <Footer pagNumb={pagNumb} setPagNumb={setPagNumb} paginationUsers={paginationUsers} />
     </div>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import "../styles/footer.css";
 
-const Footer = () => {
+const Footer = ({ pagNumb, setPagNumb, paginationUsers }) => {
+  const next = () => {
+    setPagNumb(pagNumb + 1);
+  };
+  const prev = () => {
+    setPagNumb(pagNumb - 1);
+  };
+  const selectNum = (e) => {
+    const number = parseInt(e.target.textContent);
+    setPagNumb(number - 1);
+  };
   return (
-    <footer>
-      <ul>
+    <ul className='pagination__list'>
 
-      </ul>
-    </footer>
+      {pagNumb <= 0 ? <></> : <li onClick={prev} className='pagination__item'>Prev</li>}
+
+      {paginationUsers.map((num, index) => (
+        <li key={index} onClick={selectNum} className='pagination__item'>{index + 1}</li>
+      ))}
+      {pagNumb >= 4 ? <></> : <li onClick={next} className='pagination__item'>Next</li>}
+    </ul>
   );
 };
 
